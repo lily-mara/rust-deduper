761fe3a58590c4d629a1615926e4ca362c88d2fd
Nate Mara <natemara@gmail.com>
2015-02-06 23:06:30 -0500

    show file hashes for current directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+extern crate crypto;
+
+use std::old_io::{File, fs, FileType};
+use std::old_io::fs::PathExtensions;
+use crypto::md5::Md5;
+use crypto::digest::Digest;
+
 fn main() {
-    println!("Hello, world!");
+    let folder = fs::walk_dir(&Path::new("."));
+    match folder {
+        Ok(results) => {
+            for file_path in results {
+                if file_path.is_file() {
+                    hash_file(&file_path);
+                }
+            }
+        },
+        Err(e) => println!("{}", e),
+    }
+}
+
+fn hash_file(p: &Path) {
+    let mut md5 = Md5::new();
+    println!("{}", p.display());
+    let contents = File::open(p).read_to_end();
+    match contents {
+        Ok(good) => {
+            md5.input(&good);
+            println!("{}", md5.result_str());
+        },
+        Err(i) => println!("{}", i),
+    }
 }
